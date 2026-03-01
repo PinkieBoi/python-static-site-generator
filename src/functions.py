47a5210488621103ffb047acf505a30e317734db
James MacPherson <jtdmacpherson@pinkieboi.com>
2026-03-01 17:59:58 +0000

    add split nodes by delimiter

--- src/functions.py
+++ src/functions.py
@@ -17,7 +17,21 @@ def text_node_to_html_node(text_node: TextNode):
         case TextType.LINK:
             return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode(tag="img", props={"src": text_node.url, "alt": text_node.text})
+            return LeafNode(tag="img", value=None, props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError("Invalid TextType")
 
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    all_new_nodes = []
+    for node in old_nodes:
+        if node.text.count(delimiter) == 0 or node.text.count(delimiter) % 2 != 0:
+            new_nodes = [node]
+        else:
+            split_text = node.text.split(delimiter)
+            node1 = TextNode(text=split_text[0], text_type=node.text_type)
+            d_node = TextNode(text=split_text[1], text_type=text_type)
+            node3 = TextNode(text=split_text[2], text_type=node.text_type)
+            new_nodes = [node1, d_node, node3]
+        all_new_nodes.extend(new_nodes)
+    return all_new_nodes
--- src/test_functions.py
+++ src/test_functions.py
@@ -1,5 +1,5 @@
 import unittest
-from functions import text_node_to_html_node
+from functions import text_node_to_html_node, split_nodes_delimiter
 from textnode import TextNode, TextType
 
 
@@ -14,3 +14,20 @@ class TestTextNodeToHTMLNode(unittest.TestCase):
         with self.assertRaises(ValueError):
             node = TextNode("This is a text node", text_type="plaintext")
             text_node_to_html_node(node)
+
+class TestSplitNodesDelimiter(unittest.TestCase):
+    def test_split_nodes_delimiter(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_output = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT)
+        ]
+        self.assertEqual(split_nodes, expected_output)
+
+    def test_no_match(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        split_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        expected_output = [node]
+        self.assertEqual(split_nodes, expected_output)
