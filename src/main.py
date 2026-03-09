68e3745ab81a2a53079ffc037b830a3dbf1ba6af
James MacPherson <jtdmacpherson@pinkieboi.com>
2026-03-09 06:40:31 +0000

    generate public dir

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
-from textnode import TextNode, TextType
+import os
+import shutil
+
+
+def create_public():
+    if os.path.exists("public"):
+        shutil.rmtree("public")
+    shutil.copytree("static", "public")
+
 
 def main():
-    tn = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+    root_dir = os.path.abspath(".")
+    if not os.path.exists(root_dir + "/static"):
+        os.mkdir(root_dir + "/static")
+    create_public()
 
 
 if __name__ == '__main__':
