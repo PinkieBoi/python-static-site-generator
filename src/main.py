75adecb395efb3e9a9d38cd497a98909b3e23ae8
James MacPherson <jtdmacpherson@pinkieboi.com>
2026-03-12 10:57:08 +0000

    generate ditrectory structure

--- src/main.py
+++ src/main.py
@@ -3,13 +3,18 @@ import shutil
 from functions import generate_page
 
 
+def find_files(directory, files):
+    return [file for file in files if os.path.isfile(os.path.join(directory, file))]
+
 def create_public():
     if os.path.exists("public"):
         shutil.rmtree("public")
-    shutil.copytree("static", "public")
+    shutil.copytree("content", "public", ignore=find_files)
+    shutil.copytree("static", "public", dirs_exist_ok=True)
 
 
 def convert_pages():
+    # TODO: Change to recursively gen all pages in content dir
     generate_page(
         "content/index.md",
         "template.html",
@@ -18,9 +23,9 @@ def convert_pages():
 
 
 def main():
-    root_dir = os.path.abspath(".")
-    if not os.path.exists(root_dir + "/static"):
-        os.mkdir(root_dir + "/static")
+    # root_dir = os.path.abspath(".")
+    if not os.path.exists("static"):
+        os.mkdir("static")
     create_public()
     convert_pages()
 
